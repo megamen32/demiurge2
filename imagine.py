4e67ad3ceba0e6dbd7dbf6b73485a1605970ebe3
roomhackerfrom100 <megamen932@gmai.com>
2023-10-26 23:52:41 +0300

    updated midjouerney

--- imagine.py
+++ imagine.py
@@ -35,6 +35,7 @@ async def generate_image_midjourney(prompt):
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=payload, headers=headers) as resp:
             response_json = await resp.json()
+            print(resp.status,resp.content)
             url = response_json["latest_image_url"]
 
     async with aiohttp.ClientSession() as session:
